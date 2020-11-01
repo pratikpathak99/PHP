6f738d619360b8f5ea8ac8e511bba20c3e384f99
Pratik Pathak <46027754+pratikpathak99@users.noreply.github.com>
2020-11-01 12:29:50 +0530

    Create index.php

---
+++ Assignment/index.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: asus
+ * Date: 11/1/2020
+ * Time: 11:58 AM
+ */
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>1st Assignment</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<div class="jumbotron text-center">
+    <h1>Pratik Pathak</h1>
+    <p>PHP Assignment</p>
+</div>
+
+<div class="container">
+    <div class="row">
+        <div class="col-sm-4">
+            <h3><a href="one.php">Question 1 </a></h3>
+            <p>Write a PHP script to create one variable $department and assign a value ‘Indus University’. Show value of $department variable into the title section, body section within h1 tag, h3 tag.</p>
+            <p>To understand the basic concepts of programming with PHP.</p>
+        </div>
+        <!--<div class="col-sm-4">
+            <h3>Column 2</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+            <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+        </div>
+        <div class="col-sm-4">
+            <h3>Column 3</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+            <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+        </div>-->
+    </div>
+</div>
+
+</body>
+</html>
+
