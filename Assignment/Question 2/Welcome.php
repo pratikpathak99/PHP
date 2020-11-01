8ce945c57fc884d47f58c2d436e909ccb8fc26da
Pratik Pathak <46027754+pratikpathak99@users.noreply.github.com>
2020-11-01 14:24:40 +0530

    Create Welcome.php

---
+++ Assignment/Question 2/Welcome.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: asus
+ * Date: 11/1/2020
+ * Time: 1:48 PM
+ */
+?>
+<html>
+<head>
+    <title>Welcome Page</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <style>
+        h2.headertekst {
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+<div class="jumbotron text-center">
+    <h1>Pratik Pathak</h1>
+    <p>Question 2 out-put</p>
+</div>
+
+<h2 class="headertekst">Welcome Page</h2>
+
+</body>
+</html>
+
+
