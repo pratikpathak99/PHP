6948115ee3bc19cb04576d1b4cf150cce6d4732a
Pratik Pathak <46027754+pratikpathak99@users.noreply.github.com>
2021-01-25 15:03:30 +0530

    Update mark.php

--- Assignment/Question 10/mark.php
+++ Assignment/Question 10/mark.php
@@ -1,52 +1,52 @@
 <?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "practical";
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "practical";
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
-
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
 ?>
 <html>
-<head>
-    <title>Question 10</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-    <style>
-        .center {
-            margin: 0;
-            position: absolute;
-            top: 80%;
-            left: 50%;
-            -ms-transform: translate(-50%, -50%);
-            transform: translate(-50%, -50%);
-        }
-    </style>
-</head>
-<body>
-<div class="jumbotron text-center">
-    <h1>Pratik Pathak</h1>
-    <p>Add Student Marks </p>
-</div>
-<form method="post">
-    <select name="owner">
-        <?php
-        $sql = mysqli_query($conn, "SELECT `name` FROM `student_details`");
-        while ($row = $sql->fetch_assoc()){
-            echo "<option value=\"owner1\">" . $row['name'] . "</option>";
-        }
-        ?>
-    </select>
-
-</form>
-</body>
+    <head>
+        <title>Question 10</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    </head>
+    <body>
+        <div class="jumbotron text-center">
+            <h1>Pratik Pathak</h1>
+            <p>Add Student Marks </p>
+        </div>
+        <form method="post">
+            <div class="container">
+                <table class="table table-condensed ">
+                    <thead>
+                        <tr>
+                            <th>Select Name :- </th>
+                            <th>
+                                <select name="owner">
+                                    <?php
+                                    $sql = mysqli_query($conn, "SELECT `name` FROM `student_details`");
+                                    while ($row = $sql->fetch_assoc()){
+                                        echo "<option value=\"owner1\">" . $row['name'] . "</option>";
+                                    }
+                                    ?>
+                                </select>
+                            </th>
+                        </tr>
+                    </thead>
+                </table>
+            </div>
+        </form>
+    </body>
 </html>
 
