9045159687f95a15a09314d9f8924e70cd4f353e
Pratik Pathak <46027754+pratikpathak99@users.noreply.github.com>
2021-01-26 20:18:15 +0530

    Update mark.php

--- Assignment/Question 10/mark.php
+++ Assignment/Question 10/mark.php
@@ -3,6 +3,7 @@
     $username = "root";
     $password = "";
     $dbname = "practical";
+    $Found = "";
 
     // Create connection
     $conn = new mysqli($servername, $username, $password, $dbname);
@@ -15,8 +16,35 @@
     if(isset($_POST["submit"])){
         //echo '<script>alert("Button is work")</script>';
         $student=$_POST["owner"];
+        $sql = "SELECT `id` FROM `student_details` WHERE `name` = '".$student."'";
+        $id = $conn->query($sql);
+        if ($id->num_rows > 0) {
+            // output data of each row
+            $Found="1";
+            while($row = $id->fetch_assoc()) {
+                $student_id= "" . $row["id"]."";
+            }
+        } else {
+            echo '<script>alert("Student Not Found")
+                    window.location="mark.php";
+                    </script>';
+        }
+        if($Found=="1"){
+            $Python  = $_POST["pythonmarks"];
+            $PHP = $_POST["phpmarks"];
+            $OOAD= $_POST["OOADmarks"];
+            $STQ = $_POST["STQAmarks"];
 
-        echo $student;
+            $insertsql = "INSERT INTO `marks`(`student_id`, `STQA`, `OOAD`, `PHP`, `IP`) VALUES ('".$student_id."','".$STQ."','".$OOAD."','".$PHP."','".$Python."')";
+            if ($conn->query($insertsql) === TRUE) {
+                echo '<script>alert("New record created successfully")
+                        window.location="Question%2010.php";
+                        </script>';
+            } else {
+                echo "Error: " . $sql . "<br>" . $conn->error;
+            }
+        }
+        $conn->close();
     }
 ?>
 <html>
@@ -43,6 +71,7 @@
                                 <select name="owner">
                                     <?php
                                     $sql = mysqli_query($conn, "SELECT `name` FROM `student_details`");
+                                    echo '<option>Select</option>';
                                     while ($row = $sql->fetch_assoc()){
                                         echo "<option value=" . $row['name'] . ">" . $row['name'] . "</option>";
                                     }
@@ -57,31 +86,32 @@
                         <tr>
                             <td>Python Marks :- </td>
                             <td>
-                                <input type="form-control" class="form-control" id="pythonmarks" placeholder="Enter Your Python Marks">
+                                <input type="form-control" class="form-control" id="pythonmarks" placeholder="Enter Your Python Marks" name="pythonmarks">
                                 <small class="form-text text-muted">Marks is out of 100</small>
                             </td>
                         </tr>
                         <tr>
                             <td>PHP Marks :- </td>
                             <td>
-                                <input type="form-control" class="form-control" id="phpmarks" placeholder="Enter Your PHP Marks">
+                                <input type="form-control" class="form-control" id="phpmarks" placeholder="Enter Your PHP Marks" name="phpmarks">
                                 <small class="form-text text-muted">Marks is out of 100</small>
                             </td>
                         </tr>
                         <tr>
                             <td>OOAD Marks :- </td>
                             <td>
-                                <input type="form-control" class="form-control" id="OOADmarks" placeholder="Enter Your OOAD Marks">
+                                <input type="form-control" class="form-control" id="OOADmarks" placeholder="Enter Your OOAD Marks" name="OOADmarks">
                                 <small class="form-text text-muted">Marks is out of 100</small>
                             </td>
                         </tr>
                         <tr>
                             <td>STQA Marks :- </td>
                             <td>
-                                <input type="form-control" class="form-control" id="STQAmarks" placeholder="Enter Your STQA Marks">
+                                <input type="form-control" class="form-control" id="STQAmarks" placeholder="Enter Your STQA Marks" name="STQAmarks">
                                 <small class="form-text text-muted">Marks is out of 100</small>
                             </td>
                         </tr>
+
                         <tr>
                             <td colspan="2" style="text-align: center"><button type="submit" class="btn btn-primary" name="submit" id="submit">Submit</button> </td>
                         </tr>
