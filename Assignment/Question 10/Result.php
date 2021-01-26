3998444914e5df73c821b6092abf82a25a3b9f0b
Pratik Pathak <46027754+pratikpathak99@users.noreply.github.com>
2021-01-26 20:32:54 +0530

    Create Result.php

---
+++ Assignment/Question 10/Result.php
@@ -0,0 +1,25 @@
+<?php?>
+<html>
+<head>
+    <title>Question 10</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <style>
+        .center {
+            margin: 0;
+            position: absolute;
+            top: 80%;
+            left: 50%;
+            -ms-transform: translate(-50%, -50%);
+            transform: translate(-50%, -50%);
+        }
+    </style>
+</head>
+<body>
+    <div class="jumbotron text-center">
+        <h1>Result</h1>
+    </div>
+
+</body>
+</html>
