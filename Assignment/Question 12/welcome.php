297af482df29f7a3c00cf59329c6d8cf363666d0
Pratik Pathak <46027754+pratikpathak99@users.noreply.github.com>
2021-01-30 20:55:43 +0530

    Update welcome.php

--- Assignment/Question 12/welcome.php
+++ Assignment/Question 12/welcome.php
@@ -1,1 +1,209 @@
-<?php?>
+<?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "practical";
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    // Check connection
+    if ($conn->connect_error) {
+        echo '<script>alert("Connection failed")</script>';
+    }
+
+    $sql = "SELECT `log_id`, `E_id`, `Entery_date` FROM `employee_login_log`";
+
+    $result = $conn->query($sql);
+
+?>
+<html>
+<head>
+    <title>Question 12</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <style>
+        body {font-family: Arial, Helvetica, sans-serif;}
+
+        /* Full-width input fields */
+        input[type=text], input[type=password] {
+            width: 100%;
+            padding: 12px 20px;
+            margin: 8px 0;
+            display: inline-block;
+            border: 1px solid #ccc;
+            box-sizing: border-box;
+        }
+
+        /* Set a style for all buttons */
+        button {
+            background-color:  #002147 ;
+            color: white;
+            padding: 14px 20px;
+            margin: 8px 0;
+            border: none;
+            cursor: pointer;
+            width: 100%;
+        }
+
+        button:hover {
+            opacity: 0.8;
+        }
+
+        /* Extra styles for the cancel button */
+        .cancelbtn {
+            width: auto;
+            padding: 10px 18px;
+            background-color: #f44336;
+        }
+
+        /* Center the image and position the close button */
+        .imgcontainer {
+            text-align: center;
+            margin: 24px 0 12px 0;
+            position: relative;
+        }
+
+        img.avatar {
+            width: 40%;
+            border-radius: 50%;
+        }
+
+        .container {
+            padding: 16px;
+        }
+
+        span.psw {
+            float: right;
+            padding-top: 16px;
+        }
+
+        /* The Modal (background) */
+        .modal {
+            display: none; /* Hidden by default */
+            position: fixed; /* Stay in place */
+            z-index: 1; /* Sit on top */
+            left: 0;
+            top: 0;
+            width: 100%; /* Full width */
+            height: 100%; /* Full height */
+            overflow: auto; /* Enable scroll if needed */
+            background-color: rgb(0,0,0); /* Fallback color */
+            background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
+            padding-top: 60px;
+        }
+
+        /* Modal Content/Box */
+        .modal-content {
+            background-color: #fefefe;
+            margin: 5% auto 15% auto; /* 5% from the top, 15% from the bottom and centered */
+            border: 1px solid #888;
+            width: 80%; /* Could be more or less, depending on screen size */
+        }
+
+        /* The Close Button (x) */
+        .close {
+            position: absolute;
+            right: 25px;
+            top: 0;
+            color: #000;
+            font-size: 35px;
+            font-weight: bold;
+        }
+
+        .close:hover,
+        .close:focus {
+            color: red;
+            cursor: pointer;
+        }
+
+        /* Add Zoom Animation */
+        .animate {
+            -webkit-animation: animatezoom 0.6s;
+            animation: animatezoom 0.6s
+        }
+
+        @-webkit-keyframes animatezoom {
+            from {-webkit-transform: scale(0)}
+            to {-webkit-transform: scale(1)}
+        }
+
+        @keyframes animatezoom {
+            from {transform: scale(0)}
+            to {transform: scale(1)}
+        }
+
+        /* Change styles for span and cancel button on extra small screens */
+        @media screen and (max-width: 300px) {
+            span.psw {
+                display: block;
+                float: none;
+            }
+            .cancelbtn {
+                width: 100%;
+            }
+        }
+        h2.headertekst {
+            text-align: center;
+        }
+        .center {
+            margin: 0;
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            -ms-transform: translate(-50%, -50%);
+            transform: translate(-50%, -50%);
+        }
+    </style>
+</head>
+<body>
+<div class="jumbotron text-center">
+    <h1>Pratik Pathak</h1>
+    <p>Question 12 out-put</p>
+</div>
+<h2 class="headertekst">Show Login Logs</h2>
+
+<div class="center">
+    <button onclick="document.getElementById('id01').style.display='block'" style="width:auto;">logs</button>
+</div>
+<div id="id01" class="modal">
+
+    <form class="modal-content animate" method="post">
+        <table class="table">
+            <thead>
+            <tr>
+                <th scope="col">Employee Id</th>
+                <th scope="col">Login Date</th>
+            </tr>
+            </thead>
+            <tbody>
+            <?php
+            while($row= $result->fetch_assoc()) {
+                $count = 1 ;?>
+                <tr>
+                    <td><?php $eid="".$row["E_id"].""; echo $eid; ?></td>
+                    <td><?php $date="".$row["Entery_date"].""; echo $date; ?></td>
+                </tr>
+            <?php
+
+            }
+            ?>
+            </tbody>
+        </table>
+
+    </form>
+</div>
+
+<script>
+    // Get the modal
+    var modal = document.getElementById('id01');
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+</script>
+</body>
+</html>
