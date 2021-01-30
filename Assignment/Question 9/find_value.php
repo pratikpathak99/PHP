774176617a1e66fa14f90c7355e98ed49d5cc55e
Pratik Pathak <46027754+pratikpathak99@users.noreply.github.com>
2021-01-30 21:46:49 +0530

    Create find_value.php

---
+++ Assignment/Question 9/find_value.php
@@ -0,0 +1,29 @@
+<?php
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Question 9</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <style>
+        .center {
+            margin: 0;
+            position: absolute;
+            top: 80%;
+            left: 50%;
+            -ms-transform: translate(-50%, -50%);
+            transform: translate(-50%, -50%);
+        }
+    </style>
+</head>
+<body>
+<div class="jumbotron text-center">
+    <h1>Pratik Pathak</h1>
+    <p>Question 9 out-put</p>
+</div>
+<div class="container">
+</div>
+</body>
+</html>
