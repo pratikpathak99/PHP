a7f9a7ad7915ed60bc0fdf3a29ff29b4ba2a2aaf
Pratik Pathak <46027754+pratikpathak99@users.noreply.github.com>
2021-01-30 21:08:37 +0530

    Create Question 9.php

---
+++ Assignment/Question 9/Question 9.php
@@ -0,0 +1,41 @@
+<?php?>
+
+<html>
+<head>
+    <title>Question 1</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+</head>
+<body>
+    <div class="jumbotron text-center">
+        <h1>Pratik Pathak</h1>
+        <p>Question 9 out-put</p>
+    </div>
+    <div class="container">
+    <table class="table">
+        <thead>
+        <tr>
+            <th scope="col" colspan="3" style="text-align:center;">3 * 3 Array</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+        </tr>
+        <tr>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+        </tr>
+        <tr>
+            <td>Larry</td>
+            <td>the Bird</td>
+            <td>@twitter</td>
+        </tr>
+        </tbody>
+    </table></div>
+</body>
+</html>
