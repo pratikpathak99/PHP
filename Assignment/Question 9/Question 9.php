5b82ed6480b57da455ce48607d27a0d3a2ff8c7c
Pratik Pathak <46027754+pratikpathak99@users.noreply.github.com>
2021-01-30 21:15:13 +0530

    Update Question 9.php

--- Assignment/Question 9/Question 9.php
+++ Assignment/Question 9/Question 9.php
@@ -2,7 +2,7 @@
 
 <html>
 <head>
-    <title>Question 1</title>
+    <title>Question 9</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
@@ -21,19 +21,19 @@
         </thead>
         <tbody>
         <tr>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
+            <td><input type="text" id="oneone" name="one" placeholder="Row 1 value 1" /> </td>
+            <td><input type="text" id="onetwo" name="one" placeholder="Row 1 value 2" /> </td>
+            <td><input type="text" id="onethree" name="one" placeholder="Row 1 value 3" /> </td>
         </tr>
         <tr>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
+            <td><input type="text" id="twoone" name="one" placeholder="Row 2 value 1" /> </td>
+            <td><input type="text" id="twotwo" name="one" placeholder="Row 2 value 2" /> </td>
+            <td><input type="text" id="twothree" name="one" placeholder="Row 2 value 3" /> </td>
         </tr>
         <tr>
-            <td>Larry</td>
-            <td>the Bird</td>
-            <td>@twitter</td>
+            <td><input type="text" id="threeone" name="one" placeholder="Row 3 value 1" /> </td>
+            <td><input type="text" id="threetwo" name="one" placeholder="Row 3 value 2" /> </td>
+            <td><input type="text" id="threethree" name="one" placeholder="Row 3 value 3" /> </td>
         </tr>
         </tbody>
     </table></div>
