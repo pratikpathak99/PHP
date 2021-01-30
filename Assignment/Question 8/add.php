571df3dea44d42c7c2928d117d49b8aa118aba91
Pratik Pathak <46027754+pratikpathak99@users.noreply.github.com>
2021-01-31 00:00:22 +0530

    Update add.php

--- Assignment/Question 8/add.php
+++ Assignment/Question 8/add.php
@@ -21,9 +21,39 @@
     </style>
 </head>
 <body>
-<div class="jumbotron text-center">
-    <h1>Pratik Pathak</h1>
-    <p>Question 8 out-put</p>
-</div>
+    <div class="jumbotron text-center">
+        <h1>Pratik Pathak</h1>
+        <p>Question 8 out-put</p>
+    </div>
+    <table class="table">
+        <thead>
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">First</th>
+            <th scope="col">Last</th>
+            <th scope="col">Handle</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+        </tr>
+        <tr>
+            <th scope="row">2</th>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+        </tr>
+        <tr>
+            <th scope="row">3</th>
+            <td>Larry</td>
+            <td>the Bird</td>
+            <td>@twitter</td>
+        </tr>
+        </tbody>
+    </table>
 </body>
 </html>
