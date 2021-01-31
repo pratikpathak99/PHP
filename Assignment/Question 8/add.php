7ea35ae478ffbcb09f88564e66ec9a888b170f67
Pratik Pathak <46027754+pratikpathak99@users.noreply.github.com>
2021-01-31 18:06:00 +0530

    Update add.php

--- Assignment/Question 8/add.php
+++ Assignment/Question 8/add.php
@@ -1,7 +1,14 @@
 <?php
+    session_start();
+    if(isset($_POST['submit'])){
+        $product = array("Product_ID" => $_POST["Product_Id"],"Product_Name" => $_POST["product_name"],"Product Packing_Date" =>$_POST["packing_date"],"Expiry_Date"=>$_POST["Expiry_Date"],"Quality_Of_Product" =>$_POST["Quality_Of_Product"]);
 
+        $_SESSION["Main_array"]=$product;
+        echo '<script>alert("Data Add")
+                window.location.href = "Question 8.php";
+            </script>';
 
-
+    }
 ?>
 <html>
 <head>
@@ -24,36 +31,42 @@
     <div class="jumbotron text-center">
         <h1>Pratik Pathak</h1>
         <p>Question 8 out-put</p>
+        <p>Add Product</p>
     </div>
-    <table class="table">
-        <thead>
-        <tr>
-            <th scope="col">#</th>
-            <th scope="col">First</th>
-            <th scope="col">Last</th>
-            <th scope="col">Handle</th>
-        </tr>
-        </thead>
-        <tbody>
-        <tr>
-            <th scope="row">1</th>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
-        </tr>
-        <tr>
-            <th scope="row">2</th>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
-        </tr>
-        <tr>
-            <th scope="row">3</th>
-            <td>Larry</td>
-            <td>the Bird</td>
-            <td>@twitter</td>
-        </tr>
-        </tbody>
-    </table>
+    <form method="POST">
+        <div class="container">
+            <table class="table" >
+                <thead>
+                <tr>
+                    <th scope="col">Product Id :- </th>
+                    <th scope="col" > <input type="text" name="Product_Id" id="Product_Id" placeholder="Enter Your Product Id"/> </th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <th scope="col">Product Name :- </th>
+                    <th> <input type="text" name="product_name" id="product_name" placeholder="Enter Your Product Name"/> </th>
+                </tr>
+                <tr>
+                    <th scope="row">Packing Date :- </th>
+                    <td><input type="Date" name="packing_date" id="packing_date" placeholder="Enter packing date"/></td>
+                </tr>
+                <tr>
+                    <th scope="row">Expiry Date :- </th>
+                    <td><input type="Date" name="Expiry_Date" id="Expiry_Date" placeholder="Enter Expiry Date"/></td>
+                </tr>
+                <tr>
+                    <th scope="row">Quality Of Product :- </th>
+                    <td><input type="text" name="Quality_Of_Product" id="Quality_Of_Product" placeholder="Enter Quality Of Product"/></td>
+                </tr>
+                <tr>
+                    <th colspan="2" style="text-align:center;"><input type="submit" id="submit" name="submit" class="btn btn-primary"/> </th>
+                </tr>
+
+                </tbody>
+            </table>
+        </div>
+    </form>
 </body>
 </html>
+
